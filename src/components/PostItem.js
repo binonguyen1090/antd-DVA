16293116c3febc7c086107cba349c6477ad0c51a
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-08 16:02:13 -0500

    create service and fetch data from firebase

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './PostItem.css'
 
 function PostItem(props) {
-    const {title, key, category} = props.post
+    const {title, category} = props.post
     return (
         <div className={styles.normal} style={{ padding: 20 }}>
             This is from components/PostItem
